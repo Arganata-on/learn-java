a30c4fbd207297aac55a8402190c259241191e18
Arganata <arganata.on@gmail.com>
2025-02-24 20:13:17 +0700

    Initial commit

---
+++ src/ContohSoal.java
@@ -0,0 +1,10 @@
+// Buatlah program Java yang mendeklarasikan sebuah array 2 dimensi dengan
+// ukuran 3x3. Isi array tersebut dengan bilangan bulat dari 1 hingga 9.
+// Tampilkan semua elemen dalam array tersebut menggunakan nested loop (loop
+// bersarang).
+
+public class ContohSoal {
+    public static void main(String[] args) {
+
+    }
+}
