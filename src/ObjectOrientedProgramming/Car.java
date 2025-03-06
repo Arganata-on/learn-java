f1d694788febf5d057da9f0a8c66bb8cab2be75e
Arganata <arganata.on@gmail.com>
2025-03-06 15:21:17 +0700

    Delete Car.java

--- src/ObjectOrientedProgramming/Car.java
+++
@@ -1,9 +0,0 @@
-package ObjectOrientedProgramming;
-
-public class Car {
-    String make = "Ford";
-    String model = "Mustang";
-    int year = 2025;
-    double price = 58000.99;
-    boolean isRunning = false;
-}
