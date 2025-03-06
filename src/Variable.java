8b29feef0245e31d6efb49404fa094b0294b6755
Arganata <arganata.on@gmail.com>
2025-03-06 15:46:08 +0700

    Create Variable.java

---
+++ src/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args) {
+        // variable = reuseable container for a value
+        // a variable behaves as if it was the value it contain
+
+        // Primitive = simple value stored directly in memory (stack)
+        // Reference = memory address (stack) that points to the (heap)
+
+        // Primitive = int, double, char, boolean
+        // Reference = String, array, object
+
+        // 2 Steps to creating a variable
+        // ------------------------------
+        // 1. declaration
+        // 2.assignment
+
+        
+    }
+
+}
