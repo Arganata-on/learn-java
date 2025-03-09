4ad53021bd647e3ba2fcaa4be13b1c071b1003d2
Arganata <arganata.on@gmail.com>
2025-03-09 22:29:52 +0700

    Update ifStatement.java

--- src/ifStatement.java
+++ src/ifStatement.java
@@ -5,10 +5,24 @@ public class ifStatement {
         // if statement = performs a block of code if its condition is true
 
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
 
         System.out.print("Enter your age: ");
         int age = scanner.nextInt();
 
+        System.out.print("Are you a student (true/false): ");
+        boolean isStudent = scanner.nextBoolean();
+
+        // GROUP 1
+
+        if (name.isEmpty()) {
+            System.out.println("You didn't enter your name!");
+        } else {
+            System.out.println("\nHello " + name + "!");
+        }
+
+        // GROUP 2
         if (age >= 65) {
             System.out.println("You are a senior!");
         } else if (age >= 18) {
@@ -19,6 +33,13 @@ public class ifStatement {
             System.out.println("You are a baby!");
         } else {
             System.out.println("You are a child!");
+        }
+
+        // GROUP 3
+        if (isStudent) {
+            System.out.println("You are a student!");
+        } else {
+            System.out.println("You are NOT a student!");
         }
 
         scanner.close();
