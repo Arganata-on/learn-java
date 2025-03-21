49807af7aa5d785ce0fa2d15f02fd44f6442c41d
Arganata <arganata.on@gmail.com>
2025-03-21 08:46:41 +0700

    Update Constructors.java

--- src/Constructors.java
+++ src/Constructors.java
@@ -4,11 +4,14 @@ public class Constructors {
         // constructor = A special method to initialize object
         // You can pass arguments to a constructor
         // and set up initial values
-        
-        Student student1 = new Student("Arganata", 21, 3.2);
-        Student student2 = new Student("Josh", 10, 3.2);
 
-        System.out.println(student1.name);
-        System.out.println(student2.name);
+        Student student1 = new Student("Arganata", 21, 3.5, true);
+        Student student2 = new Student("Josh", 18, 3.2, true);
+        Student student3 = new Student("Mathew", 22, 3.8, false);
+
+        student1.study();
+        student2.study();
+        student3.study();
+
     }
 }
