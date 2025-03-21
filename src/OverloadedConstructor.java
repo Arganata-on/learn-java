3390ecf047e965dcbf0f58a36e5cece0ac6af595
Arganata <arganata.on@gmail.com>
2025-03-21 09:59:40 +0700

    Update OverloadedConstructor.java

--- src/OverloadedConstructor.java
+++ src/OverloadedConstructor.java
@@ -1,3 +1,11 @@
 public class OverloadedConstructor {
+    public static void main(String[] args) {
+        Mobil mobil = new Mobil();
 
+        mobil.setModel("Test");
+        mobil.setSpeed(210);
+
+        mobil.info();
+
+    }
 }
