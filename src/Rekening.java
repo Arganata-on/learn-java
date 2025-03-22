d5b092a135aae882c8c22f34870ae453b2e0bdbe
Arganata <arganata.on@gmail.com>
2025-03-22 09:18:31 +0700

    Update Rekening.java

--- src/Rekening.java
+++ src/Rekening.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
-
 public class Rekening {
     private String nomorRekening;
-    private String namaPemilik;
-    private double saldo;
+    private String namaPemilik; 
+    private double saldo; 
 
     public void setNomorRekening(String nomorRekening) {
         this.nomorRekening = nomorRekening;
@@ -14,7 +12,7 @@ public class Rekening {
     }
 
     public void setSaldo(double saldo) {
-        if (saldo >= 0) { // Validasi untuk saldo awal
+        if (saldo >= 0) { 
             this.saldo = saldo;
         } else {
             System.out.println("Saldo tidak boleh negatif!");
@@ -33,20 +31,7 @@ public class Rekening {
         return saldo;
     }
 
-    // Method untuk deposit
     public void deposit(double jumlah) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.printf("Masukkan jumlah deposit: %s", jumlah);
-        jumlah = scanner.nextDouble();
-
-        if (jumlah > 100.000) {
-            saldo += jumlah;
-        } else {
-            System.out.println("Jumlah deposit kurang dari minimal!");
-        }
-        scanner.close();
-
         if (jumlah > 0) {
             saldo += jumlah;
             System.out.println("Deposit berhasil: Rp " + jumlah);
@@ -55,7 +40,6 @@ public class Rekening {
         }
     }
 
-    // Method untuk withdraw
     public void withdraw(double jumlah) {
         if (jumlah > 0 && jumlah <= saldo) {
             saldo -= jumlah;
@@ -67,9 +51,8 @@ public class Rekening {
         }
     }
 
-    // Method untuk menampilkan informasi rekening
     public void info() {
-        System.out.println("Nomor Rekening: " + nomorRekening);
+        System.out.println("\nNomor Rekening: " + nomorRekening);
         System.out.println("Nama Pemilik: " + namaPemilik);
         System.out.println("Saldo: Rp " + saldo);
     }
