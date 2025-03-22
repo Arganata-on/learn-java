144a5cf2e3d0ee0a562389c7f7d9316cce899a0b
Arganata <arganata.on@gmail.com>
2025-03-22 08:55:50 +0700

    Create SistemRekeningBank.java

---
+++ src/SistemRekeningBank.java
@@ -0,0 +1,10 @@
+public class SistemRekeningBank {
+    public static void main(String[] args) {
+        Rekening rekening = new Rekening();
+
+        rekening.setNomorRekening("12345");
+        rekening.setNamaPemilik("Arganata");
+        rekening.setSaldo(0);
+
+    }
+}
