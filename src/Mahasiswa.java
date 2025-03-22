25ab840858b2eab5f3acb66ea78abdc83b6400d2
Arganata <arganata.on@gmail.com>
2025-03-22 08:55:45 +0700

    Create Mahasiswa.java

---
+++ src/Mahasiswa.java
@@ -0,0 +1,29 @@
+public class Mahasiswa {
+    private String nama;
+    private String nomerIndukMahasiswa;
+    private String jurusan;
+
+    public String getNam() {
+        return nama;
+    }
+
+    public String getNIM() {
+        return nomerIndukMahasiswa;
+    }
+
+    public String getJur() {
+        return jurusan;
+    }
+
+    public void setNam(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNIM(String nomerIndukMahasiswa) {
+        this.nomerIndukMahasiswa = nomerIndukMahasiswa;
+    }
+
+    public void setJur(String jurusan) {
+        this.jurusan = jurusan;
+    }
+}
