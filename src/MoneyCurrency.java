95a874b4853f89080c48653e1d037d497807f3a2
Arganata <arganata.on@gmail.com>
2025-04-13 22:46:34 +0700

    Create MoneyCurrency.java

---
+++ src/MoneyCurrency.java
@@ -0,0 +1,10 @@
+public class MoneyCurrency {
+    public static void main(String[] args) {
+        double ammount = 10000;
+
+        System.out.printf("Dollar: $%,.2f%n", ammount);
+        System.out.printf("Rupiah: Rp%,.2f%n", ammount);
+
+    }
+
+}
