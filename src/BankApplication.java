9b1927a26987a57fa4fd9374e86014c45fdf9480
Arganata <arganata.on@gmail.com>
2025-04-13 22:54:19 +0700

    Update BankApplication.java

--- src/BankApplication.java
+++ src/BankApplication.java
@@ -1,12 +1,12 @@
 public class BankApplication {
     public static void main(String[] args) {
         BankAccount ba = new BankAccount();
-        ba.setNomorRekening(1);
-        ba.setNamaPemilik("Arga");
+        ba.setNomorRekening(123);
+        ba.setNamaPemilik("Handika Rado Arganata");
 
         System.out.println("Nomor rekening: " + ba.getNomorRekening());
         System.out.println("Nama pemilik: " + ba.getNamaPemilik());
-        System.out.println("Saldo awal: " + ba.getSaldo());
+        System.out.printf("Saldo awal: Rp%,.2f%n", ba.getSaldo());
 
         // ba.deposit(10000);
         ba.withdraw(2000);
