67127fc57773b1ea3ea4e73da0ee09b03726fb2a
Arganata <arganata.on@gmail.com>
2025-05-14 15:20:34 +0700

    Create BangunRuang.java

---
+++ src/UTS/BangunRuang.java
@@ -0,0 +1,32 @@
+package UTS;
+
+public class BangunRuang {
+    private int rusuk;
+    private double panjang, lebar, tinggi, jari, volume, luas;
+
+    public BangunRuang(int rusuk) {
+        this.rusuk = rusuk;
+    }
+
+    public BangunRuang(int panjang, int lebar, int tinggi) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.tinggi = tinggi;
+    }
+
+    public void menghitungVolumeKubus() {
+        volume = Math.pow(rusuk, 3);
+        System.out.println("Volume kubus: " + volume);
+    }
+
+    public void menghitungLuasKubus() {
+        luas = 6 * (Math.pow(rusuk, 2));
+        System.out.println("Luas kubus: " + luas);
+    }
+
+    public void menghitungVolumeBalok() {
+        volume = panjang * lebar * tinggi;
+        System.out.println(volume);
+    }
+
+}
