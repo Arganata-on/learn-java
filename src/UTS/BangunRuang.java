53891ca45f218842cc2a9e9c7730429dfae489c3
Arganata <arganata.on@gmail.com>
2025-05-14 16:58:49 +0700

    Update BangunRuang.java

--- src/UTS/BangunRuang.java
+++ src/UTS/BangunRuang.java
@@ -21,31 +21,32 @@ public class BangunRuang {
 
     public void menghitungVolumeKubus() {
         volume = Math.pow(rusuk, 3);
-        System.out.println("Volume kubus: " + volume);
+        System.out.println("\nVolume kubus: " + volume + "cm³\n");
     }
 
     public void menghitungLuasKubus() {
         luas = 6 * (Math.pow(rusuk, 2));
-        System.out.println("Luas kubus: " + luas);
+        System.out.println("\nLuas kubus: " + luas + "cm³\n");
     }
 
     public void menghitungVolumeBalok() {
         volume = panjang * lebar * tinggi;
-        System.out.println(volume);
+        System.out.println("\nVolume balok: " + volume + "cm³\n");
     }
 
     public void menghitungLuasBalok() {
         luas = 2 * ((panjang * lebar) + (panjang * tinggi) + (lebar * tinggi));
-        System.out.println(luas);
+        System.out.println("\nLuas balok: " + luas + "cm³\n");
     }
 
     public void menghitungVolumeSilinder() {
         volume = Math.PI * Math.pow(jari, 2) * tinggi;
-        System.out.println(volume);
+        System.out.println("\nVolume silinder: " + volume + "cm³\n");
     }
 
     public void menghitungLuasSilinder() {
         luas = 2 * Math.PI * jari * (jari + tinggi);
+        System.out.println("\nLuas silinder: " + luas + "cm³\n");
     }
 
 }
