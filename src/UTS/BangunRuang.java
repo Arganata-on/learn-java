13aea8a3c752b95dd6954469f821eb64cb58768f
Arganata <arganata.on@gmail.com>
2025-05-15 12:19:14 +0700

    Update BangunRuang.java

--- src/UTS/BangunRuang.java
+++ src/UTS/BangunRuang.java
@@ -3,64 +3,77 @@ package UTS;
 public class BangunRuang {
     int rusuk;
     double panjang, lebar, tinggi, jari, volume = 0, luas = 0;
+    String nama;
 
     public BangunRuang(int rusuk) {
+        nama = "Kubus";
         this.rusuk = rusuk;
     }
 
     public BangunRuang(int panjang, int lebar, int tinggi) {
+        nama = "Balok";
         this.panjang = panjang;
         this.lebar = lebar;
         this.tinggi = tinggi;
     }
 
     public BangunRuang(double jari, int tinggi) {
+        nama = "Silinder";
         this.jari = jari;
         this.tinggi = tinggi;
     }
 
     public BangunRuang(double jari) {
+        nama = "Bola";
         this.jari = jari;
     }
 
     public void menghitungVolumeKubus() {
         volume = Math.pow(rusuk, 3);
-        System.out.println("\nVolume kubus: " + volume + "cm³\n");
+        showVolume();
     }
 
     public void menghitungLuasKubus() {
         luas = 6 * (Math.pow(rusuk, 2));
-        System.out.println("\nLuas kubus: " + luas + "cm³\n");
+        showLuas();
     }
 
     public void menghitungVolumeBalok() {
         volume = panjang * lebar * tinggi;
-        System.out.println("\nVolume balok: " + volume + "cm³\n");
+        showVolume();
     }
 
     public void menghitungLuasBalok() {
         luas = 2 * ((panjang * lebar) + (panjang * tinggi) + (lebar * tinggi));
-        System.out.println("\nLuas balok: " + luas + "cm³\n");
+        showLuas();
     }
 
     public void menghitungVolumeSilinder() {
         volume = Math.PI * Math.pow(jari, 2) * tinggi;
-        System.out.println("\nVolume silinder: " + volume + "cm³\n");
+        showVolume();
     }
 
     public void menghitungLuasSilinder() {
         luas = 2 * Math.PI * jari * (jari + tinggi);
-        System.out.println("\nLuas silinder: " + luas + "cm³\n");
+        showLuas();
     }
 
     public void menghitungVolumeBola() {
         volume = 4.0 / 3.0 * Math.PI * Math.pow(jari, 3);
-        System.out.println("Volume bola: " + volume + "cm³\n");
+        showVolume();
     }
 
     public void menghitungLuasBola() {
         luas = 4 * Math.PI * Math.pow(jari, 2);
-        System.out.println("Luas bola: " + luas);
+        showLuas();
+    }
+
+    public void showVolume() {
+        System.out.println("\nVolume " + nama + ": " + volume + "cm³\n");
+    }
+
+    public void showLuas() {
+        System.out.println("\nLuas " + nama + ": " + luas + "cm²\n");
     }
 
 }
