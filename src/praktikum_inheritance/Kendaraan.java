91af7acbccbbf9cac3213740ec318bec99f7e011
Arganata <arganata.on@gmail.com>
2025-06-01 13:46:15 +0700

    Create Kendaraan.java

---
+++ src/praktikum_inheritance/Kendaraan.java
@@ -0,0 +1,18 @@
+package praktikum_inheritance;
+
+public class Kendaraan {
+    String mesin;
+
+    public String getMesin() {
+        return mesin;
+    }
+
+    public void setMesin(String mesin) {
+        this.mesin = mesin;
+    }
+
+    public void jalankan() {
+        System.out.println("Dimasukkan kompling");
+    }
+
+}
